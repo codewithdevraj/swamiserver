d0fb4eb4cd2a04cf78a824eabcde5e3bbdd03c06
🌟🔮𝕯𝖊𝖛𝖗𝖆𝖏📚 <dev.raj.697492@gmail.com>
2024-10-31 02:14:43 +0530

    Add files via upload

--- controllers/auth.js
+++ controllers/auth.js
@@ -76,7 +76,7 @@ const handlelogin = async (req, res) => {
     res.cookie("token", token, {
       maxAge: 24 * 60 * 60 * 1000,
       // httpOnly: false, // Prevent access from client-side JavaScript
-      sameSite: "None", // Allow cross-site requests (important for CORS)
+      // sameSite: "None", // Allow cross-site requests (important for CORS)
       // secure: true, // Ensure the cookie is sent over HTTPS only
       //   domain: "swamiserver.onrender.com", // Ensure it's tied to your server domain
       //   path: "/", // Make the cookie accessible to the entire app
@@ -85,7 +85,7 @@ const handlelogin = async (req, res) => {
     res.cookie("sessionId", sessionId, {
       maxAge: 24 * 60 * 60 * 1000,
       // httpOnly: false,
-      sameSite: "None",
+      // sameSite: "None",
       // secure: true,
       //   domain: "swamiserver.onrender.com",
       //   path: "/",
